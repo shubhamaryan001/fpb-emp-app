b22388277b5bb5cd994083c9fd1c89764f152a13
Shubham Saurabh <saurabharyan30@gmail.com>
2021-04-14 17:34:59 +0530

    first commit

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,4 @@
 // export const API = "http://localhost:8000/api";
-export const API = "https://shubhamaryan001-fpb-emp-backend-3622.zeet.app/api";
+// export const API = "https://shubhamaryan001-fpb-emp-backend-3622.zeet.app/api";
+
+export const API = "http://nakshabazaar.in/api";
